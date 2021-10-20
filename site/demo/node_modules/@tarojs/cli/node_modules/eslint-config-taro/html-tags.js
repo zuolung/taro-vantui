314f1853f12a1ac8b53f3220999350d0770f0411
zuolung@126.com <apple@AppledeMacBook-Pro.local>
2021-10-20 08:54:25 +0800

    remove node_modules

--- site/demo/node_modules/@tarojs/cli/node_modules/eslint-config-taro/html-tags.js
+++
@@ -1,9 +0,0 @@
-// elements-of-html
-// https://github.com/w3c/elements-of-html
-
-module.exports = [
-  {
-    element: 'select',
-    message: '请使用 Picker 组件代替 <select>'
-  }
-]
--- site/demo/node_modules/postcss-html-transform/README.md
+++
@@ -1,3 +0,0 @@
-# `postcss-html-transform`
-
-
